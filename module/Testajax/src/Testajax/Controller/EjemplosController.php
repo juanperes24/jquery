38cbae899d73d86d5aed2b8ea9f05e91e2b08a9b
juanperes24 <juan_peres_24@hotmail.com>
2013-11-15 18:42:53 -0400

    1
    con algunos ejemplos

---
+++ module/Testajax/src/Testajax/Controller/EjemplosController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Testajax\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\Form\Annotation\AnnotationBuilder;
+use Testajax\Model\TestEntity;
+
+class EjemplosController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+
+    public function formAction()
+    {
+        return array();
+    }
+    
+    public function formfecAction()
+    {
+        return array();
+    }
+}
+
